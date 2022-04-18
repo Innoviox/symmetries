cb77df42b3343d84f5f53d2ed615cec772e6abb3
Simon Chervenak <sarras305@gmail.com>
2022-04-17 23:08:51 -0400

    a boy and his cube
    thank you 11th grade simon for writing this webpack stuff for me

--- js/index.js
+++ js/index.js
@@ -6,22 +6,72 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xd9d5ca);
+
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
-
 const controls = new OrbitControls(camera, renderer.domElement);
 
-//controls.update() must be called after any manual changes to the camera's transform
 camera.position.set(0, 20, 100);
 controls.update();
 
-const geometry = new THREE.BoxGeometry();
-const edges = new THREE.EdgesGeometry(geometry);
+function make_sided_material(piece) {
+    const material = new THREE.MeshBasicMaterial({
+        vertexColors: true
+    });
+    const colors = [];
+
+    const color = new THREE.Color();
+
+    for (let i = 0; i < 3; i += 6) {
+
+        color.setHex(0xffffff * Math.random());
+
+        colors.push(color.r, color.g, color.b);
+        colors.push(color.r, color.g, color.b);
+        colors.push(color.r, color.g, color.b);
+
+        colors.push(color.r, color.g, color.b);
+        colors.push(color.r, color.g, color.b);
+        colors.push(color.r, color.g, color.b);
+    } // for
+
+    // define the new attribute
+    piece.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
+
+    return material;
+}
+
+const cube = new THREE.BoxGeometry(10, 10, 10);
+const cubeMesh = new THREE.Mesh(cube, make_sided_material(cube));
+cubeMesh.position.set(0, 0, 0);
+const edges = new THREE.EdgesGeometry(cube);
 const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0xffffff }));
 scene.add(line);
 
+let spheres = [];
+spheres.push(new THREE.Mesh(new THREE.SphereGeometry(1), new THREE.MeshBasicMaterial({ color: 0xffffff })));
+spheres.push(new THREE.Mesh(new THREE.SphereGeometry(1), new THREE.MeshBasicMaterial({ color: 0xffff00 })));
+spheres.push(new THREE.Mesh(new THREE.SphereGeometry(1), new THREE.MeshBasicMaterial({ color: 0xff00ff })));
+spheres.push(new THREE.Mesh(new THREE.SphereGeometry(1), new THREE.MeshBasicMaterial({ color: 0x00ffff })));
+spheres.push(new THREE.Mesh(new THREE.SphereGeometry(1), new THREE.MeshBasicMaterial({ color: 0xff0000 })));
+spheres.push(new THREE.Mesh(new THREE.SphereGeometry(1), new THREE.MeshBasicMaterial({ color: 0x0000ff })));
+spheres.push(new THREE.Mesh(new THREE.SphereGeometry(1), new THREE.MeshBasicMaterial({ color: 0x00ff00 })));
+spheres.push(new THREE.Mesh(new THREE.SphereGeometry(1), new THREE.MeshBasicMaterial({ color: 0xff0000 })));
+
+spheres[0].position.set(cubeMesh.position.x + 5, cubeMesh.position.y + 5, cubeMesh.position.z + 5);
+spheres[1].position.set(cubeMesh.position.x - 5, cubeMesh.position.y + 5, cubeMesh.position.z + 5);
+spheres[2].position.set(cubeMesh.position.x + 5, cubeMesh.position.y - 5, cubeMesh.position.z + 5);
+spheres[3].position.set(cubeMesh.position.x - 5, cubeMesh.position.y - 5, cubeMesh.position.z + 5);
+spheres[4].position.set(cubeMesh.position.x + 5, cubeMesh.position.y + 5, cubeMesh.position.z - 5);
+spheres[5].position.set(cubeMesh.position.x - 5, cubeMesh.position.y + 5, cubeMesh.position.z - 5);
+spheres[6].position.set(cubeMesh.position.x + 5, cubeMesh.position.y - 5, cubeMesh.position.z - 5);
+spheres[7].position.set(cubeMesh.position.x - 5, cubeMesh.position.y - 5, cubeMesh.position.z - 5);
 
 
+for (let i = 0; i < spheres.length; i++) {
+    scene.add(spheres[i]);
+}
 
 function animate() {
 
