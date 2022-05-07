7fc7c931326e8bf4e3fd331f7b3a6b7317751dfe
Simon Chervenak <sarras305@gmail.com>
2022-05-07 17:57:30 -0400

    finish adding progress & clean

--- js/tour.js
+++ js/tour.js
@@ -10,58 +10,60 @@ function make_tour() {
             },
             classes: 'shadow-md bg-white',
             scrollTo: true,
-            // when: {
-            //     show() {
-            //         const currentStepElement = tour.currentStep.el;
-            //         const footer = currentStepElement.querySelector('.shepherd-footer');
-            //         const progress = document.createElement('span');
-            //         progress.className = 'shepherd-progress';
-            //         progress.innerText = `${tour.steps.indexOf(tour.currentStep) + 1} of ${tour.steps.length}`;
-            //         footer.insertBefore(progress, tour.querySelector('.shepherd-button:last-child'));
-            //     }
-            // }
+            when: {
+                show() {
+                    const currentStepElement = tour.currentStep.el;
+                    const footer = currentStepElement.querySelector('.shepherd-footer');
+                    const progress = document.createElement('span');
+                    progress.className = 'shepherd-progress';
+                    progress.innerText = `${tour.steps.indexOf(tour.currentStep) + 1} of ${tour.steps.length}`;
+                    footer.insertBefore(progress, currentStepElement.querySelector('.shepherd-button:last-child'));
+                }
+            }
         },
     });
 
-    tour.addStep({
+    let steps = [new Shepherd.Step(tour, {
         id: 'step1',
         text: 'Welcome to Symmetries! Here we are investigating the symmetries of the cube. While you might think this is just permuting the vertices, this leads to twisting the cube, and we only want rotations that preserve the cube\'s structure. Therefore, we must turn to the diagonals.',
         buttons: [{ text: 'Next', action: tour.next }],
-    });
+    }),
 
-    tour.addStep({
+    new Shepherd.Step(tour, {
         id: 'step2',
         text: 'Here you can see the elements of the symmetric group S4. Click on one (after the tour) to apply it.',
         attachTo: { element: '#sigmatable', on: 'left' },
         buttons: [{ text: 'Next', action: tour.next }],
-    });
+    }),
 
-    tour.addStep({
+    new Shepherd.Step(tour, {
         id: 'step3',
         text: 'Here you can see the cube in its original state. Click and drag to rotate.',
         attachTo: { element: '#container', on: 'right' },
         buttons: [{ text: 'Next', action: tour.next }],
-    });
+    }),
 
-    tour.addStep({
+    new Shepherd.Step(tour, {
         id: 'step4',
         text: 'Here you can toggle the cube on and off to see the diagonals.',
         attachTo: { element: '#switch', on: 'left' },
         buttons: [{ text: 'Next', action: tour.next }],
-    });
+    }),
 
-    tour.addStep({
+    new Shepherd.Step(tour, {
         id: 'step5',
         text: 'Here you can see how each permutation corresponds to a rotation of the cube.',
         attachTo: { element: '#rot-table', on: 'left' },
         buttons: [{ text: 'Next', action: tour.next }],
-    });
+    }),
 
-    tour.addStep({
+    new Shepherd.Step(tour, {
         id: 'step6',
         text: 'Try different values of σ to see why permuting the diagonals leads to all the symmetries of the cube.',
         buttons: [{ text: 'Next', action: tour.next }],
-    });
+    })];
+
+    tour.addSteps(steps);
 
     tour.on('complete', () => {
         toggle_enabled('sigma');
